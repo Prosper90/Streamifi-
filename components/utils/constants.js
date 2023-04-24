987811f02eb92fb17f21fcd53233197e4a41c1e6
Prosper <nkonyeprospero@gmail.com>
2023-04-25 00:58:08 +0100

    wrapping up

--- components/utils/constants.js
+++ components/utils/constants.js
@@ -4,7 +4,7 @@ import abi from "./abi.json";
 //localhost  
 //testnet 0x511B75e0230Ab35dFc0C1a7E0569AB636f3DB8fC
 //main 
-export const ContractAddress = "0x511B75e0230Ab35dFc0C1a7E0569AB636f3DB8fC";
+export const ContractAddress = "0x01A8BB127d889B3CF7be95CA3Ab84aeBbe91B1F2";
 
 
 export const contractABI = abi;
